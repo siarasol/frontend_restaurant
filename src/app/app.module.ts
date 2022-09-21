dcfcd3595467a773e5a330dc8171a552210f00d7
siarasol <squirogaf@fcpn.edu.bo>
2022-09-21 18:55:59 -0400

    create proy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      InicioComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
